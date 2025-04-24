601463d3b5041fdd0a2f6fdbc1f11d3a827b3a27
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-04-24 17:46:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,18 @@ import './App.css'
 function App() {
   return (
     <div>
-      {sayHello}
+      <Profile name ="Aravind" age={21} />
     </div>
-  )
+  );
 }
-const sayHello = () => {return "Hello"}
+function Profile (props){
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+    </div>
+  );
+} 
+
 
 export default App
